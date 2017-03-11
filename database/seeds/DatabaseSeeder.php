8f0dc61cf33392dde5c4d2308fdf94185657ff19
Kevin Compton <kevincompton@Kevins-iMac.local>
2017-03-10 16:57:53 -0800

    seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Event::class, 10)->create();
+        factory(App\Rize::class, 10)->create();
     }
 }
